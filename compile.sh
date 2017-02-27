8bf6eeee5051a7bac90b32549a035c47bb149f79
Bradley Walters <zerfgog202@gmail.com>
2017-02-26 17:52:14 -0700

    Modify compile script to wrap output in an IIFE

--- compile.sh
+++ compile.sh
@@ -1,1 +1,6 @@
-java -jar closure_compiler.jar --js all.js --js_output_file compiled.js --compilation_level ADVANCED
+#!/bin/sh
+java -jar closure_compiler.jar \
+	--js all.js \
+	--js_output_file compiled.js \
+	--compilation_level ADVANCED \
+	--output_wrapper "(function(global) {%output%})(typeof global !== 'undefined' ? global : this);"
